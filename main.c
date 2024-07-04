f12930e8c9308ec0282cd481678ae8717e70dc84
dleiferives <dleiferives@gmail.com>
2024-07-03 22:59:02 -0700

    Added serialization to the user
    Will have to come up with a versioning system as I work,
    So that as different versions of the project happen it can understand the different forms of the serialized files

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 // TODO
-// Implement Lists
 // If the user wants to resize their serialized data, then all of the references will have to be updated
 // this is because the fragments, and the names all use the offsets into that pool for their data
+// Implement timing for the user, and the fragments
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -83,6 +83,26 @@ void List_add(List_t * list, void * data){
 	list->size++;
 }
 
+void List_serialize_to_file(List_t * list, FILE * file){
+	fwrite(&list->size, sizeof(u32), 1, file);
+	fwrite(&list->capacity, sizeof(u32), 1, file);
+	fwrite(&list->type, sizeof(Type_k), 1, file);
+	fwrite(&list->member_size, sizeof(size_t), 1, file);
+	fwrite(list->data, list->member_size, list->size, file);
+}
+
+List_t List_deserialize_from_file(FILE * file){
+	List_t list = {0};
+	fread(&list.size, sizeof(u32), 1, file);
+	fread(&list.capacity, sizeof(u32), 1, file);
+	fread(&list.type, sizeof(Type_k), 1, file);
+	fread(&list.member_size, sizeof(size_t), 1, file);
+	list.data = calloc(list.capacity, list.member_size);
+	assert(list.data != NULL, "Could not allocate list data");
+	fread(list.data, list.member_size, list.size, file);
+	return list;
+}
+
 enum Fragment_e{
 	FRAGMENT_TYPE_IN_ANSWER,	
 	FRAGMENT_COUNT,
@@ -109,6 +129,40 @@ struct Fragment_s{
 	FragmentData_t data;
 };
 
+void Fragment_serialize_to_file(Fragment_t * fragment, FILE * file){
+	fwrite(&fragment->id, sizeof(u32), 1, file);
+	fwrite(&fragment->vessel_id, sizeof(u32), 1, file);
+	fwrite(&fragment->creation, sizeof(time_t), 1, file);
+	fwrite(&fragment->challenge_occurance, sizeof(time_t), 1, file);
+	fwrite(&fragment->kind, sizeof(Fragment_k), 1, file);
+	switch(fragment->kind){
+		case FRAGMENT_TYPE_IN_ANSWER:
+		fwrite(&fragment->data.type_in_answer.front, sizeof(StrId), 1, file);
+		fwrite(&fragment->data.type_in_answer.back, sizeof(StrId), 1, file);
+		break;
+		default:
+		break;
+	}
+}
+
+Fragment_t Fragment_deserialize_from_file(FILE * file){
+	Fragment_t fragment = {0};
+	fread(&fragment.id, sizeof(u32), 1, file);
+	fread(&fragment.vessel_id, sizeof(u32), 1, file);
+	fread(&fragment.creation, sizeof(time_t), 1, file);
+	fread(&fragment.challenge_occurance, sizeof(time_t), 1, file);
+	fread(&fragment.kind, sizeof(Fragment_k), 1, file);
+	switch(fragment.kind){
+		case FRAGMENT_TYPE_IN_ANSWER:
+		fread(&fragment.data.type_in_answer.front, sizeof(StrId), 1, file);
+		fread(&fragment.data.type_in_answer.back, sizeof(StrId), 1, file);
+		break;
+		default:
+		break;
+	}
+	return fragment;
+}
+
 /// Vessel struct definition
 /// user_id holds the user id, if 0 there is no associated user
 /// id is the vessel id, if it is 0 this is an invalid vessel
@@ -119,6 +173,23 @@ struct Vessel_s {
 	StrId name;
 	List_t fragment_ids;
 };
+
+void Vessel_serialize_to_file(Vessel_t * vessel, FILE * file){
+	fwrite(&vessel->id, sizeof(u32), 1, file);
+	fwrite(&vessel->user_id, sizeof(u32), 1, file);
+	fwrite(&vessel->name, sizeof(StrId), 1, file);
+	List_serialize_to_file(&vessel->fragment_ids, file);
+}
+
+Vessel_t Vessel_deserialize_from_file(FILE * file){
+	Vessel_t vessel = {0};
+	fread(&vessel.id, sizeof(u32), 1, file);
+	fread(&vessel.user_id, sizeof(u32), 1, file);
+	fread(&vessel.name, sizeof(StrId), 1, file);
+	vessel.fragment_ids = List_deserialize_from_file(file);
+	return vessel;
+}
+
 
 
 /// User struct definition
@@ -132,6 +203,24 @@ struct User_s{
 	List_t fragments;
 	List_t vessels;
 };
+
+void User_serialize_to_file(User_t * user, FILE * file){
+	fwrite(&user->id, sizeof(u32), 1, file);
+	fwrite(&user->memory_strength, sizeof(float), 1, file);
+	InternPool_serialize_to_file(user->pool, file);
+	List_serialize_to_file(&user->fragments, file);
+	List_serialize_to_file(&user->vessels, file);
+}
+
+User_t User_deserialize_from_file(FILE * file){
+	User_t user = {0};
+	fread(&user.id, sizeof(u32), 1, file);
+	fread(&user.memory_strength, sizeof(float), 1, file);
+	user.pool = InternPool_deserialize_from_file(file);
+	user.fragments = List_deserialize_from_file(file);
+	user.vessels = List_deserialize_from_file(file);
+	return user;
+}
 
 void User_vessels_add(User_t *user, StrId name){
 	assert(user != NULL, "User cannot be null when creating a vessel");
@@ -206,6 +295,24 @@ void User_fragments_print(User_t *user){
 	}
 }
 
+void User_serialize(User_t *user, u8 *file){
+	FILE *f = fopen(file, "wb");
+	User_serialize_to_file(user, f);
+	fclose(f);
+}
+
+User_t User_deserialize(u8 *file){
+	FILE *f = fopen(file, "rb");
+	User_t user = User_deserialize_from_file(f);
+	fclose(f);
+	return user;
+}
+
+
+
+
+
+
 
 int main(void){
 	// InternPool_t * pool = InternPool_new();
@@ -242,6 +349,12 @@ int main(void){
 
 	User_vessels_print(&user);
 	User_fragments_print(&user);
+
+	User_serialize(&user, "user.bin");
+
+	User_t new_user = User_deserialize("user.bin");
+
+	User_vessels_print(&new_user);
 
 	
 
